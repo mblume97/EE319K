aadce4e38f29f1b8f86096b105e5a7d9617d0cf8
mblume97 <mblume@utexas.edu>
2017-10-10 13:24:28 -0600

    rf

--- lab1/lab1.d
+++
@@ -1,2 +0,0 @@
-.\lab1.o: Lab1.c
-.\lab1.o: C:\Users\Michael\Desktop\New folder\ARM\ARMCC\bin\..\include\stdint.h
--- lab1/main.d
+++
@@ -1,3 +0,0 @@
-.\main.o: main.c
-.\main.o: C:\Users\Michael\Desktop\New folder\ARM\ARMCC\bin\..\include\stdint.h
-.\main.o: UART.h
